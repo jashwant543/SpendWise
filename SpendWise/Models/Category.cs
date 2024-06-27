bb39837dcad06d909b7986a4390e3cffa2767aa2
Jashwant Kumar <singh943235humpty@gmail.com>
2024-06-27 15:23:38 +0530

    Added get and post ,method for adding and deleting expense/income Improved UI of the applciation

--- SpendWise/Models/Category.cs
+++ SpendWise/Models/Category.cs
@@ -17,8 +17,18 @@ namespace SpendWise.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
-
-        
-       
+     
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
+
+
+
+
 }
--- SpendWise/Program.cs
+++ SpendWise/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllersWithViews();
 //DI
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+/// License registration
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2U1hhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5VdEJjW31Wc3ZQRmlY");
 
 
 var app = builder.Build();
